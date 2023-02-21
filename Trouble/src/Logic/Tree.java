3e6447de4a5fd2ad8d52cec0ead5e9fa33c9aa8d
Sbressette1 <bressette@unbc.ca>
2023-02-21 13:36:14 -0800

    commit more like kermit

--- Trouble/src/Logic/Tree.java
+++ Trouble/src/Logic/Tree.java
@@ -1,5 +1,13 @@
 package Logic;
 
 public class Tree {
+
+    public static void main(String[] args) {
+
+        for(int i = 1; i < 100; i ++){
+            System.out.println("Deez");
+        }
+
+    }
     
 }
