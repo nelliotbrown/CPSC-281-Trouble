2ed9b4613e303a7aef2083d3b1cf87c4683da5ec
Noah <nelliotbrown@gmail.com>
2023-02-21 14:30:14 -0800

    Adding crap

--- Trouble/src/Logic/Moves.java
+++ Trouble/src/Logic/Moves.java
@@ -1,5 +1,42 @@
 package Logic;
 
 public class Moves {
+    private int weight;
+
+
+    
+    
+    /**
+     * finds the weight of a move for a given piece based on a dice roll & the current board 
+     */
+    public int findMoves(Dice roll, Board b, Piece p, Start s){
+        if(roll == 6 && start != 0){
+
+
+        }
+
+        switch (roll, b) {
+            case value:
+                
+                break;
+        
+            default:
+                break;
+        }
+
+
+        return this.weight;
+    } 
+    /*
+    PRIORITIES
+    1. GETTING HOME
+    2. CLEARING START TILE
+    3. MOVING FROM HOME TO START TILE
+    4. KILLING AN OPPONENT
+    5. MOVE
+    Illegal moves will be assigned negative weights
+    */
+
+
     
 }
--- Trouble/src/Test/App.java
+++ Trouble/src/Test/App.java
@@ -1,4 +1,5 @@
 package Test;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
