ccc859df29b12dd89198ac7cc1d5dd704d339011
LFiddler <Spikelovesbigmacs1!>
2023-03-06 14:09:20 -0800

    First commit

---
+++ Trouble/src/Players.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public class Players {
+    private Random rand = new Random();
+
+    public Players(){
+
+    }
+
+    public int rollDice(){
+        return rand.nextInt(1,7);
+    }
+}
