8ab0ac5c115c3a8a341d4da2c7ae36af48764ef2
Noah <nelliotbrown@gmail.com>
2023-03-20 10:33:36 -0700

    Refined moves to update weights 3, 4 & 5

--- Trouble/src/Logic/Moves.java
+++ Trouble/src/Logic/Moves.java
@@ -9,6 +9,8 @@ public class Moves {
     private Pieces[] boardstate;
     private Pieces colour;
     private Board board;
+    private int startPos;
+    private int endPos;
     
     
 
@@ -22,51 +24,71 @@ public class Moves {
 
     
     /**
-     * finds the weight of a move for a given piece based on a dice roll & the current board 
+     * updates the weight of a move for a given piece based on a dice roll & the current board 
+     * still missing cases where the weight is 1, 2, 3
      * @param roll from 1-6
-     * @param b the board from it's current state
-     * @param p the given piece
      * @return
      */
-    public int findMoves(){
+    public void updateWeight(){
 
         if(roll != 6 && inStart())
             this.weight = -1;
         //assigns a negative weight to trying to leave the start when anything but a 6 is not rolled
 
-
-        
         if(!(inStart())){
             switch (roll) {
-                case roll = 1:
-                    break;
+                case 1:
+                if(isOccupied(this.startPos + 1)){
+                    this.weight = 4;
+                }
+                this.weight = 5;
+                break;
 
-                case roll = 2:
-                    break;
+                case 2:
+                if(isOccupied(this.startPos + 2) ){
+                    this.weight = 4;
+                }
+                this.weight = 5;
+                break;
                 
-                case roll = 3:
-                    break;
+                case 3:
+                if(isOccupied(this.startPos + 3) ){
+                    this.weight = 4;
+                }
+                this.weight = 5;
+                break;
                     
-                case roll = 4:
-                    break;
+                case 4:
+                if(isOccupied(this.startPos + 4) ){
+                    this.weight = 4;
+                }
+                this.weight = 5;
+                break;
                 
-                case roll = 5:
-                    break;
+                case 5:
+                if(isOccupied(this.startPos + 5) ){
+                    this.weight = 4;
+                }
+                this.weight = 5;
+                break;
                 
-                case roll = 6:
-                    break;    
+                case 6:
+                if(isOccupied(this.startPos + 6) ){
+                    this.weight = 4;
+                }
+                this.weight = 5;
+                break;    
             }
         }else{
+            this.weight = 3;
             
         }
 
-
-        return this.weight;
     } 
     /*
-    PRIORITIES
-    1. GETTING HOME
-    2. CLEARING START TILE
+    WEIGHT VALUES BASED ON POSSIBLE MOVES
+    1. GETTING HOME ?????
+    2. CLEARING START TILE ?????
     3. MOVING FROM HOME TO START TILE
     4. KILLING AN OPPONENT
     5. MOVE
@@ -80,6 +102,13 @@ public class Moves {
         return weight;
     }
 
+    public int getStartPos(){
+        return startPos;
+    }
+    public int getEndPos(){
+        return endPos;
+    }
+
 
 
     public static Moves[] findMoves(Board b, int roll){
@@ -87,8 +116,14 @@ public class Moves {
     }
 
     public boolean inStart(){
-        return(findMoves(board, roll) == board.getSP(colour));
+        return(board.getSP(this.colour) == this.startPos);
     }
 
+    public boolean isOccupied(int i){
+        return()
+        
+    }
+
+    
 
 }
